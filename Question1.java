f4d68f145dbd07b6f021b1cb4afa9c763b50142b
SamDeeven <samdeevn@gmail.com>
2022-11-10 20:32:33 +0530

    First Commit

---
+++ Question1.java
@@ -0,0 +1,16 @@
+
+class Maths {
+public static void main(String[] args){
+   int a,b,add, subtract, multiply, divide;
+   a=10;
+   b=2;
+   add=a+b;
+   subtract=a-b;
+   multiply=a*b;
+   divide=a/b;
+   System.out.println(add);
+   System.out.println(subtract);
+   System.out.println(multiply);
+   System.out.println(divide);
+}
+}
