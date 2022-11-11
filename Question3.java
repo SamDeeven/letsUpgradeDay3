f0d150898db7e7a8bdabe7ddab0154a17186564b
SamDeeven <samdeevn@gmail.com>
2022-11-11 21:46:12 +0530

    Commit3

---
+++ Question3.java
@@ -0,0 +1,12 @@
+
+class Maths {
+   public static void main(String[] args){
+      int a=23;
+      for(int i=10;i<=150;i++){
+         if(i%23 == 0){
+            System.out.println(i);
+         }
+      }
+   }
+   }
+   
